40c4526938a7b9ca7e405fb285f5eba4b1f1817a
mazax1ct <vova@giveandget.ru>
2021-11-30 17:19:44 +0300

    main

---
+++ src/js/decor.js
@@ -0,0 +1,24 @@
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+$(document).ready(function() {
+  if($('body').width() > 1399) {
+    let i = 0;
+
+    let bH = $('main').height()/6;
+
+    while (i < 5) {
+      let img = getRandomInt(1, 17);
+      let className = 'right';
+      if((i%2) > 0) {
+        className = 'left';
+      }
+
+      $('main').append('<div class="decor '+ className +'" style="top:'+(bH*i + 160)+'px"><img src="images/backgrounds/decor/'+ img +'.png" /></div>');
+      i++;
+    }
+  }
+});
