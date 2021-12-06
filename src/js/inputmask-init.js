0b85944d0aa71b91fa5cf79d9905de75661a43c5
mazax1ct <vova@giveandget.ru>
2021-12-06 18:44:30 +0300

    consult

--- src/js/inputmask-init.js
+++ src/js/inputmask-init.js
@@ -5,4 +5,6 @@ $(document).ready(function() {
   });
 
   $(".js-email-mask").inputmask({ alias: "email", "clearIncomplete": true, showMaskOnHover: false});
+
+  $(".js-date").inputmask({ alias: "datetime", inputFormat: "dd.mm.yyyy", placeholder: "_", "clearIncomplete": true, showMaskOnHover: false});
 });
