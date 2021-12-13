39c4cff938efff59ecea40f7a3988e5af05d460d
mazax1ct <vova@giveandget.ru>
2021-12-13 18:14:09 +0300

    lab

---
+++ src/js/reviews-slider.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+  if($('.js-reviews-slider').length) {
+    $('.js-reviews-slider').each(function(index) {
+      $(this).slick({
+        arrows: true,
+        prevArrow: '<button class="slick-arrow slick-prev" aria-label="Назад" type="button"><svg class="icon" aria-hidden="true"><use xlink:href="#arrow_left"/></svg></button>',
+        nextArrow: '<button class="slick-arrow slick-next" aria-label="Вперед" type="button"><svg class="icon" aria-hidden="true"><use xlink:href="#arrow_right"/></svg></button>',
+        mobileFirst: true,
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        appendArrows: $('#' + $(this).attr('data-buttons')),
+        responsive: [
+          {
+            breakpoint: 1023,
+            settings: {
+              slidesToShow: 6,
+              slidesToScroll: 6
+            }
+          },
+          {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3
+            }
+          },
+          {
+            breakpoint: 579,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          }
+        ]
+      });
+    });
+  }
+});
