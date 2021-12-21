8f06fb62b5e8b59f800b1442ffe47bb730c083dc
mazax1ct <vova@giveandget.ru>
2021-12-21 15:57:32 +0300

    personal

---
+++ build/js/news-slider.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+  if($('.js-news-slider').length) {
+    $('.js-news-slider').each(function(index) {
+      $(this).slick({
+        arrows: true,
+        prevArrow: '<button class="slick-arrow slick-prev" aria-label="Назад" type="button"><svg class="icon" aria-hidden="true"><use xlink:href="#arrow_left"/></svg></button>',
+        nextArrow: '<button class="slick-arrow slick-next" aria-label="Вперед" type="button"><svg class="icon" aria-hidden="true"><use xlink:href="#arrow_right"/></svg></button>',
+        mobileFirst: true,
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        appendArrows: $('#' + $(this).attr('data-buttons')),
+        responsive: [
+          {
+            breakpoint: 991,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3
+            }
+          },
+          {
+            breakpoint: 579,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          }
+        ]
+      });
+    });
+  }
+});
